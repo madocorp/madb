44b06b882b56c0cc357467490239e8433e312745
Madocsai László <madocsai.laszlo@arukereso.com>
2026-06-24 19:33:33 +0200

    Field editor

--- Table/EditController.php
+++ Table/EditController.php
@@ -155,6 +155,56 @@ class EditController {
     return $prefix . ' ' . self::$schema . '.' . $table;
   }
 
+  private static function parseColumnType($columnType) {
+    $columnType = trim((string) $columnType);
+    $unsigned = stripos($columnType, ' unsigned') !== false;
+    $zerofill = stripos($columnType, ' zerofill') !== false;
+    $clean = trim(str_ireplace([' unsigned', ' zerofill'], '', $columnType));
+    if (preg_match('/^([a-z0-9]+)(?:\((.*)\))?$/i', $clean, $matches)) {
+      return [
+        'type' => strtoupper($matches[1]),
+        'parameter' => $matches[2] ?? '',
+        'unsigned' => $unsigned,
+        'zerofill' => $zerofill
+      ];
+    }
+    return [
+      'type' => strtoupper($clean),
+      'parameter' => '',
+      'unsigned' => $unsigned,
+      'zerofill' => $zerofill
+    ];
+  }
+
+  private static function buildColumnType($type, $parameter, $unsigned, $zerofill) {
+    $type = strtoupper(trim((string) $type));
+    $parameter = trim((string) $parameter);
+    $columnType = $type;
+    if ($parameter !== '') {
+      $columnType .= '(' . $parameter . ')';
+    }
+    if ($unsigned) {
+      $columnType .= ' unsigned';
+    }
+    if ($zerofill) {
+      $columnType .= ' zerofill';
+    }
+    return $columnType;
+  }
+
+  private static function selectColumnTypeInList($type) {
+    $list = \SPTK\Element::byName('column-type', self::itemPanel('table-column-editor'));
+    if ($list === false) {
+      return;
+    }
+    foreach ($list->getDescendants() as $index => $item) {
+      if ($item->getValue() === $type) {
+        $list->moveCursor($index);
+        return;
+      }
+    }
+  }
+
   private static function setTitle($title) {
     $panelTitle = \SPTK\Element::firstByType('PanelTitle', self::panel());
     if ($panelTitle !== false) {
@@ -301,7 +351,6 @@ class EditController {
       $default = $column['COLUMN_DEFAULT'] ?? '';
       $item = new \SPTK\Elements\ListItem($list);
       $item->setValue($name);
-      $item->setSelectable('table-editor-columns');
       self::addCell($item, 'w20', $name);
       self::addCell($item, 'w20', $type);
       self::addCell($item, 'w40', implode(' ', $attributes));
@@ -336,7 +385,6 @@ class EditController {
     foreach ($groupedIndexes as $name => $index) {
       $item = new \SPTK\Elements\ListItem($list);
       $item->setValue($name);
-      $item->setSelectable('table-editor-indexes');
       self::addCell($item, 'w25', $name);
       self::addCell($item, 'w25', $index['type']);
       self::addCell($item, 'w50', implode(', ', $index['columns']));
@@ -359,7 +407,6 @@ class EditController {
       $rules = 'U:' . ($foreignKey['UPDATE_RULE'] ?? '') . ' D:' . ($foreignKey['DELETE_RULE'] ?? '');
       $item = new \SPTK\Elements\ListItem($list);
       $item->setValue(self::makeItemKey([$name, $column, $targetSchema, $targetTable, $targetColumn]));
-      $item->setSelectable('table-editor-foreign-keys');
       self::addCell($item, 'w25', $name);
       self::addCell($item, 'w20', $column);
       self::addCell($item, 'w40', "{$targetSchema}.{$targetTable}.{$targetColumn}");
@@ -381,7 +428,6 @@ class EditController {
       $statement = $trigger['ACTION_STATEMENT'] ?? '';
       $item = new \SPTK\Elements\ListItem($list);
       $item->setValue($name);
-      $item->setSelectable('table-editor-triggers');
       self::addCell($item, 'w25', $name);
       self::addCell($item, 'w10', $timing);
       self::addCell($item, 'w10', $event);
@@ -429,37 +475,78 @@ class EditController {
       return;
     }
     self::$editingItem = $index;
+    $type = self::parseColumnType($column['COLUMN_TYPE'] ?? '');
+    $key = $column['COLUMN_KEY'] ?? '';
+    $extra = $column['EXTRA'] ?? '';
     $panel = self::itemPanel('table-column-editor');
     $panel->setValue([
       'column-name' => $column['COLUMN_NAME'] ?? '',
-      'column-type' => $column['COLUMN_TYPE'] ?? '',
-      'column-nullable' => $column['IS_NULLABLE'] ?? '',
+      'column-parameter' => $type['parameter'],
+      'column-primary' => $key === 'PRI',
+      'column-unique' => $key === 'UNI',
+      'column-not-null' => ($column['IS_NULLABLE'] ?? '') === 'NO',
+      'column-auto-increment' => stripos($extra, 'auto_increment') !== false,
+      'column-unsigned' => $type['unsigned'],
+      'column-zerofill' => $type['zerofill'],
       'column-default' => $column['COLUMN_DEFAULT'] ?? '',
-      'column-key' => $column['COLUMN_KEY'] ?? '',
-      'column-extra' => $column['EXTRA'] ?? '',
       'column-charset' => $column['CHARACTER_SET_NAME'] ?? '',
       'column-collation' => $column['COLLATION_NAME'] ?? '',
       'column-comment' => $column['COLUMN_COMMENT'] ?? ''
     ]);
+    self::selectColumnTypeInList($type['type']);
     self::showItemPanel($panel, 'column-name');
   }
 
   public static function saveColumnEditor($panel) {
-    if (self::$editingItem === false || !isset(self::$columns[self::$editingItem])) {
+    if (!self::applyColumnEditorValues($panel)) {
       return;
     }
+    self::closeItemEditor($panel);
+  }
+
+  public static function syncColumnEditor($element = null) {
+    $panel = self::itemPanel('table-column-editor');
+    if ($panel === false || !self::applyColumnEditorValues($panel)) {
+      return;
+    }
+    \SPTK\Element::refresh();
+  }
+
+  private static function applyColumnEditorValues($panel) {
+    if (self::$editingItem === false || !isset(self::$columns[self::$editingItem])) {
+      return false;
+    }
     $values = $panel->getValue();
+    $primary = (bool) ($values['column-primary'] ?? false);
+    $unique = (bool) ($values['column-unique'] ?? false);
+    $autoIncrement = (bool) ($values['column-auto-increment'] ?? false);
     self::$columns[self::$editingItem]['COLUMN_NAME'] = $values['column-name'] ?? '';
-    self::$columns[self::$editingItem]['COLUMN_TYPE'] = $values['column-type'] ?? '';
-    self::$columns[self::$editingItem]['IS_NULLABLE'] = $values['column-nullable'] ?? '';
+    self::$columns[self::$editingItem]['COLUMN_TYPE'] = self::buildColumnType(
+      $values['column-type'] ?? '',
+      $values['column-parameter'] ?? '',
+      (bool) ($values['column-unsigned'] ?? false),
+      (bool) ($values['column-zerofill'] ?? false)
+    );
+    self::$columns[self::$editingItem]['IS_NULLABLE'] = ($values['column-not-null'] ?? false) ? 'NO' : 'YES';
     self::$columns[self::$editingItem]['COLUMN_DEFAULT'] = $values['column-default'] ?? '';
-    self::$columns[self::$editingItem]['COLUMN_KEY'] = $values['column-key'] ?? '';
-    self::$columns[self::$editingItem]['EXTRA'] = $values['column-extra'] ?? '';
+    self::$columns[self::$editingItem]['COLUMN_KEY'] = $primary ? 'PRI' : ($unique ? 'UNI' : '');
+    self::$columns[self::$editingItem]['EXTRA'] = $autoIncrement ? 'auto_increment' : '';
     self::$columns[self::$editingItem]['CHARACTER_SET_NAME'] = $values['column-charset'] ?? '';
     self::$columns[self::$editingItem]['COLLATION_NAME'] = $values['column-collation'] ?? '';
     self::$columns[self::$editingItem]['COLUMN_COMMENT'] = $values['column-comment'] ?? '';
     self::setColumns(self::$columns);
-    self::closeItemEditor($panel);
+    $list = self::listElement('table-editor-columns');
+    if ($list !== false) {
+      $list->moveCursor(self::$editingItem);
+    }
+    return true;
+  }
+
+  public static function selectColumnType($item) {
+    $panel = self::itemPanel('table-column-editor');
+    $panel->activateInput('column-parameter');
+    self::applyColumnEditorValues($panel);
+    \SPTK\Element::refresh();
   }
 
   public static function openIndexEditor($item) {
